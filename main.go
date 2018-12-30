8743f8a61334297afe5d728d7e25dbcbecf9d258
Andrey Galaktionov <das.frama@gmail.com>
2018-12-30 19:27:03 +0300

    Перевёл на другую GUI библиотеку.

--- main.go
+++ main.go
@@ -5,181 +5,155 @@ import (
 	"log"
 	"time"
 
-	"github.com/marcusolsson/tui-go"
+	"github.com/jroimartin/gocui"
 )
+
+var headers = []string{
+	"Глава 1",
+	"Глава 2",
+	"Глава 3",
+}
 
 const (
-	caret     = "$ "
-	myName    = "frama"
-	animSpeed = 30
+	startMessage = "Sollen wir anfangen (Ja / Nein)?"
 )
 
-var ui tui.UI
-
-type uiChanel struct {
-	label    *tui.Label
-	messages []Message
-	history  *tui.Box
-	cursor   int
-}
-
-type historyWidget struct {
-	*tui.Box
-	historyBox *tui.Box
-
-	chanels []*uiChanel
-	active  int
-}
-
-func (h *historyWidget) OnKeyEvent(ev tui.KeyEvent) {
-	switch ev.Key {
-	case tui.KeyTab:
-		h.Next()
-	case tui.KeyBacktab:
-		h.Prev()
+func keybindings(g *gocui.Gui) error {
+	if err := g.SetKeybinding("", gocui.KeyEsc, gocui.ModNone, quit); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("cmd", gocui.KeyEnter, gocui.ModNone, sendCmd); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("cmd", gocui.KeyTab, gocui.ModNone, run); err != nil {
+		return err
 	}
 
-	h.Box.OnKeyEvent(ev)
+	return nil
 }
 
-func (h *historyWidget) SetChanel(chanel *uiChanel) {
-	h.historyBox.Remove(0)
-	historyScroll := tui.NewScrollArea(chanel.history)
-	historyScroll.SetAutoscrollToBottom(true)
-	h.historyBox.Append(historyScroll)
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	// Side.
+	if v, err := g.SetView("side", 0, 0, int(0.2*float32(maxX))-1, maxY-1); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Highlight = true
+		v.SelBgColor = gocui.ColorGreen
+		v.SelFgColor = gocui.ColorBlack
+		for _, header := range headers {
+			fmt.Fprintln(v, header)
+		}
+	}
+	// Main.
+	if v, err := g.SetView("main", int(0.2*float32(maxX)), 0, maxX-1, maxY-4); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		fmt.Fprintln(v, "> Приветик")
+		v.Wrap = true
+		v.Autoscroll = true
+	}
+	// Cmd.
+	if v, err := g.SetView("cmd", int(0.2*float32(maxX)), maxY-3, maxX-1, maxY-1); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		var lineEditor gocui.Editor = gocui.EditorFunc(lineEditor)
+		v.Editable = true
+		v.Editor = lineEditor
+		v.Wrap = false
+		if _, err := g.SetCurrentView("cmd"); err != nil {
+			return err
+		}
+	}
 
-	jobs := make(chan int, len(chanel.messages))
+	return nil
+}
+
+func lineEditor(v *gocui.View, key gocui.Key, ch rune, mod gocui.Modifier) {
+	switch {
+	case ch != 0 && mod == 0:
+		v.EditWrite(ch)
+	case key == gocui.KeySpace:
+		v.EditWrite(' ')
+	case key == gocui.KeyBackspace || key == gocui.KeyBackspace2:
+		v.EditDelete(true)
+	case key == gocui.KeyArrowLeft:
+		v.MoveCursor(-1, 0, false)
+	case key == gocui.KeyArrowRight:
+		v.MoveCursor(1, 0, false)
+	}
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
+}
+
+func sendCmd(g *gocui.Gui, v *gocui.View) error {
+	main, err := g.View("main")
+	if err != nil {
+		return err
+	}
+	fmt.Fprintf(main, "%s", v.ViewBuffer())
+	v.Clear()
+	v.SetCursor(0, 0)
+	return nil
+}
+
+func run(g *gocui.Gui, v *gocui.View) error {
+	go sendMsg("К моей голове подключили радио.\n", g)
+
+	return nil
+}
+
+func sendMsg(message string, g *gocui.Gui) {
+	ch := make(chan rune, len(message))
+
 	go func() {
-		for i := 0; i < len(chanel.messages); i++ {
-			h.PrintMessage(chanel.messages[i], jobs)
+		for {
+			c, more := <-ch
+			if more {
+				g.Update(func(g *gocui.Gui) error {
+					main, err := g.View("main")
+					if err != nil {
+						return err
+					}
+					fmt.Fprintf(main, "%c", c)
+
+					return nil
+				})
+			} else {
+				return
+			}
 		}
 	}()
-	for i := 0; i < len(chanel.messages); i++ {
-		jobs <- i
+
+	for _, r := range message {
+		ch <- r
+		time.Sleep(40 * time.Millisecond)
 	}
-	close(jobs)
-}
-
-func (h *historyWidget) PrintMessage(message Message, jobs chan int) {
-	_, more := <-jobs
-	if more {
-		// Задержка перед появлением сообщения.
-		if message.delay > 0 {
-			time.Sleep(time.Duration(message.delay) * time.Millisecond)
-		}
-
-		textLabel := tui.NewLabel("")
-		// textLabel.SetSizePolicy(tui.Expanding, tui.Expanding)
-		// textLabel.SetWordWrap(true)
-		h.chanels[h.active].history.Append(tui.NewHBox(
-			tui.NewLabel(fmt.Sprintf("[%s]: ", myName)),
-			textLabel,
-			tui.NewSpacer(),
-		))
-		for _, s := range message.text {
-			ui.Update(func() {
-				textLabel.SetText(textLabel.Text() + string(s))
-			})
-			time.Sleep(time.Duration(animSpeed) * time.Millisecond)
-		}
-	}
-}
-
-func (h *historyWidget) Next() {
-	h.active = clamp(h.active+1, 0, len(h.chanels)-1)
-	h.style()
-	h.SetChanel(h.chanels[h.active])
-}
-
-func (h *historyWidget) Prev() {
-	h.active = clamp(h.active-1, 0, len(h.chanels)-1)
-	h.style()
-	h.SetChanel(h.chanels[h.active])
-}
-
-func (h *historyWidget) style() {
-	for i := 0; i < len(h.chanels); i++ {
-		if i == h.active {
-			h.chanels[i].label.SetStyleName("chanel-selected")
-			continue
-		}
-		h.chanels[i].label.SetStyleName("chanel")
-	}
-}
-
-func newHistoryWidget(chanels ...*uiChanel) *historyWidget {
-	view := &historyWidget{chanels: chanels}
-
-	sidebar := tui.NewVBox()
-	sidebar.SetBorder(true)
-	sidebar.SetSizePolicy(tui.Maximum, tui.Minimum)
-	for i := 0; i < len(chanels); i++ {
-		sidebar.Append(chanels[i].label)
-	}
-	sidebar.Append(tui.NewSpacer())
-
-	historyScroll := tui.NewScrollArea(chanels[0].history)
-	historyScroll.SetAutoscrollToBottom(true)
-	historyBox := tui.NewVBox(historyScroll)
-	historyBox.SetBorder(true)
-	view.historyBox = historyBox
-
-	input := tui.NewEntry()
-	input.SetFocused(true)
-	input.SetSizePolicy(tui.Expanding, tui.Maximum)
-	inputBox := tui.NewHBox(
-		tui.NewLabel(caret),
-		input,
-	)
-	inputBox.SetBorder(true)
-	inputBox.SetSizePolicy(tui.Expanding, tui.Maximum)
-
-	chat := tui.NewVBox(historyBox, inputBox)
-	chat.SetSizePolicy(tui.Expanding, tui.Expanding)
-
-	input.OnSubmit(func(e *tui.Entry) {
-		view.chanels[view.active].history.Append(tui.NewHBox(
-			tui.NewLabel("> "),
-			tui.NewLabel(e.Text()),
-			tui.NewSpacer(),
-		))
-		input.SetText("")
-	})
-	view.Box = tui.NewHBox(sidebar, chat)
-	view.style()
-
-	return view
+	close(ch)
 }
 
 func main() {
-	historyLayout := newHistoryWidget(
-		&uiChanel{label: tui.NewLabel("#0. Пролог"), messages: Intro, history: tui.NewVBox()},
-		&uiChanel{label: tui.NewLabel("#1. В доме приглушён огонь"), messages: Test, history: tui.NewVBox()},
-		// &uiChanel{label: tui.NewLabel("#2. Пробежавший лось"), messases: &Test, history: tui.NewVBox()},
-	)
-	var err error
-	ui, err = tui.New(historyLayout)
+	g, err := gocui.NewGui(gocui.OutputNormal)
 	if err != nil {
-		log.Fatal(err)
+		log.Panicln(err)
 	}
-	ui.SetKeybinding("Esc", func() { ui.Quit() })
+	defer g.Close()
 
-	theme := tui.NewTheme()
-	theme.SetStyle("label.chanel", tui.Style{Reverse: tui.DecorationOff})
-	theme.SetStyle("label.chanel-selected", tui.Style{Reverse: tui.DecorationOn, Fg: tui.ColorRed, Bg: tui.ColorWhite})
-	ui.SetTheme(theme)
+	g.Cursor = true
+	g.InputEsc = true
 
-	if err := ui.Run(); err != nil {
-		log.Fatal(err)
-	}
-}
+	g.SetManagerFunc(layout)
 
-func clamp(n, min, max int) int {
-	if n < min {
-		return max
+	if err := keybindings(g); err != nil {
+		log.Panicln(err)
 	}
-	if n > max {
-		return min
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
 	}
-	return n
 }
